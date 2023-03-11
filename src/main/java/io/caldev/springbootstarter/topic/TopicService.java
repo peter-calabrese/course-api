de601a1e4f0827278f8df43d7e191d86064872bc
Peter Calabrese <peter@caldev.io>
2023-03-10 21:05:16 -0500

    Added post

--- src/main/java/io/caldev/springbootstarter/topic/TopicService.java
+++ src/main/java/io/caldev/springbootstarter/topic/TopicService.java
@@ -2,22 +2,27 @@ package io.caldev.springbootstarter.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 // Business Service - @service is a stereotype service, tells Spring this is a Spring Business Service
 @Service
 public class TopicService {
-List<Topic> topics = Arrays.asList(
+List<Topic> topics = new ArrayList<>(Arrays.asList(
         new Topic("spring", "Spring Framework", "Spring Framework Description"),
-                new Topic("java", "Core Java", "Core Java Description"),
+                new Topic("java ", "Core Java", "Core Java Description"),
                 new Topic("javascript", "Javascript", "Javascript Description")
-        );
+        ));
 
     public List<Topic> getAllTopics() {
         return topics;
     }
     public Topic getTopic(String id) {
         return topics.stream().filter(topic -> topic.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
     }
 }
